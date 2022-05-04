906c5377e3241d0ed07b7022f4180c437ade8b5b
Vannarith Om Jr <vannarithomjr@swuser-129-8-214-152.swlan.csufresno.edu>
2022-05-04 10:09:29 -0700

    English portion

---
+++ lab_5.py
@@ -0,0 +1,13 @@
+"""
+The program is trying to translate a sentence captured as user input.
+We first read in the text file textese.txt.
+Then from the file, we create a list of strings from each lines in the text file.
+Then, we create a dictionary the list.
+Once the text file has been read into memory, we close the file.
+
+We then define a function for translating which envolves splitting the user input (sentence) into an
+array of strings ("enjoy the excellent band tonight") ["enjoy", "the", "excellent", "band", "tonight"]
+
+After each word is translated, we then
+print out the translated sentence to the user.
+"""
